7f58cdc9743ac0da8df6fdb6a853e83c725cba84
Matt Burgess <matt@smallbatch.io>
2025-04-24 10:17:02 +0700

    Final update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Bubbles from "./assets/bubbles.svg";
 
 import Crab from "./components/crab/Crab";
 import Kelp from "./components/Kelp";
+import Wave from "./components/Wave";
 
 // import Home from "./components/sections/Home";
 import GettingStarted from "./components/sections/GettingStarted";
@@ -179,73 +180,8 @@ function App() {
         >
           <div className="absolute top-0 left-0 w-full h-[100px] overflow-hidden z-0 pointer-events-none bg-white">
             <div className="relative w-[200%] h-full ">
-              <svg
-                viewBox="0 0 1200 100"
-                preserveAspectRatio="none"
-                className="absolute top-0 left-0 w-full h-[100px] animate-waveSlow z-0"
-              >
-                <path
-                  d="
-      M0 50
-      Q 30 30, 60 50
-      T 120 50
-      T 180 50
-      T 240 50
-      T 300 50
-      T 360 50
-      T 420 50
-      T 480 50
-      T 540 50
-      T 600 50
-      T 660 50
-      T 720 50
-      T 780 50
-      T 840 50
-      T 900 50
-      T 960 50
-      T 1020 50
-      T 1080 50
-      T 1140 50
-      T 1200 50
-      V100 H0 Z
-    "
-                  fill="#3490dc"
-                />
-              </svg>
-
-              <svg
-                viewBox="0 0 1200 100"
-                preserveAspectRatio="none"
-                className="absolute top-0 left-0 w-[150%] h-[100px] z-0 opacity-70"
-              >
-                <path
-                  d="
-      M0 50
-      Q 30 30, 60 50
-      T 120 50
-      T 180 50
-      T 240 50
-      T 300 50
-      T 360 50
-      T 420 50
-      T 480 50
-      T 540 50
-      T 600 50
-      T 660 50
-      T 720 50
-      T 780 50
-      T 840 50
-      T 900 50
-      T 960 50
-      T 1020 50
-      T 1080 50
-      T 1140 50
-      T 1200 50
-      V100 H0 Z
-    "
-                  fill="#3490dc"
-                />
-              </svg>
+              <Wave className="absolute top-0 left-0 w-full h-[100px] animate-waveSlow z-0" />
+              <Wave className="absolute top-0 left-0 w-[150%] h-[100px] z-0 opacity-70" />
             </div>
           </div>
 
---
+++ src/components/Wave.jsx
@@ -0,0 +1,14 @@
+export default function Wave({ className }) {
+  return (
+    <svg
+      viewBox="0 0 1200 100"
+      preserveAspectRatio="none"
+      className={className}
+    >
+      <path
+        d=" M0 50 Q 30 30, 60 50 T 120 50 T 180 50 T 240 50 T 300 50 T 360 50 T 420 50 T 480 50 T 540 50 T 600 50 T 660 50 T 720 50 T 780 50 T 840 50 T 900 50 T 960 50 T 1020 50 T 1080 50 T 1140 50 T 1200 50 V100 H0 Z"
+        fill="#3490dc"
+      />
+    </svg>
+  );
+}
